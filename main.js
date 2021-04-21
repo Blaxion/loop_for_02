4a6c18f4d24c3cef3496b5cbb789c09173e2b0f3
Blaxion <khibaziev.f@gmail.com>
2021-04-21 23:48:30 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// // Exo 1
+// let tab = ["Farhad","Yassin","Alexis","Kevin","Junior","Andy","Tania","Issam","Mohammed", "Mihai", "Loic", "Charles", "William","Oussama","Ilyas","Anthony","Dorian","Alain","Cactus"]
+
+// tab.forEach(e => {
+//     console.log("Bonjour ", e)
+// });
+
+
+// // Exo 2
+// let tab = ["Farhad","Yassin","Alexis","Kevin","Junior"]
+
+// tab.forEach(e => {
+//     console.log(e)
+// });
